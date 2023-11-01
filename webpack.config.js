36bf740238d42c89bb9c70d25f8ecf87817227c2
Yuriy Shaklak <yurijfedor178@gmail.com>
2023-11-01 22:39:41 +0200

    update webpack_config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
-    publicPath: "/build/",
+    publicPath: "/",
   },
   module: {
     rules: [
